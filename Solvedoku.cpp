6d586bfb615e8c50379d0d2109e6ace58b4f2f80
XuperBryan <bryanbobo123@gmail.com>
2020-08-20 16:21:43 -0700

    Updated solve function

--- Solvedoku.cpp
+++ Solvedoku.cpp
@@ -10,16 +10,15 @@ struct number {
 	int count = 0;
 
 	// true equates to filled position, false equates to empty position
-	bool row [9];
-	bool column [9];
-	bool box [9];
+	bool row [9] = {false, false,false,false,false,false,false,false,false};
+	bool column [9] = {false, false,false,false,false,false,false,false,false};
+	bool box [9] = {false, false,false,false,false,false,false,false,false};
 };
 
 // list every number, indexed from 1-9
 number list[10];
 
 void printNum(int num) {
-	cout << "Count " << list[num].count << endl;
 	for (int i = 0; i < 9; i++) {
 		cout << "Row " << i+1 << ": " << list[num].row[i] << endl;
 		cout << "Column " << i+1 << ": " << list[num].row[i] << endl;
@@ -60,33 +59,27 @@ void printSudoku() {
 void solver(int num, int boxNum) {
 	/*cout << num << " " << boxNum << endl;*/
 	bool miniBox[3][3];
-	/*for (int whatevers = 0; whatevers < 3; whatevers++) {
-		for (int sindeXESareacrimeagainsthumanityandthebaneofmyexistence = 0; sindeXESareacrimeagainsthumanityandthebaneofmyexistence < 3; sindeXESareacrimeagainsthumanityandthebaneofmyexistence++) {
-			miniBox[whatevers][sindeXESareacrimeagainsthumanityandthebaneofmyexistence] = false;
+	// initialization of miniBox
+	for (int boxIs = 0; boxIs < 3; boxIs++) {
+		for (int sboxJ = 0; sboxJ < 3; sboxJ++) {
+			miniBox[boxIs][sboxJ] = false;
 		}
-		cout << endl;
-	}*/
-	if (!list[num].box[boxNum]) {
+	}
+
+	if (!(list[num].box[boxNum])) {
 		for (int r = (boxNum/3)*3; r <= (boxNum/3)*3+2; r++) {
 			for (int c = (boxNum%3)*3; c <= (boxNum%3)*3+2; c++) {
 				if(list[num].row[r]==true){
+					miniBox[r%3][c%3] = true;
+				} else if(list[num].column[c]==true){
 					miniBox[r%3][c%3] = true; 
-				}
-				if(list[num].column[c]==true){
-					miniBox[r%3][c%3] = true; 
-				}
-				if(sudoku[r][c]) {
+				} else if(sudoku[r][c]!=0) {
 					miniBox[r%3][c%3] = true;
 				}
-				/*for (int whatever = 0; whatever < 3; whatever++) {
-					for (int indeXESareacrimeagainsthumanityandthebaneofmyexistence = 0; indeXESareacrimeagainsthumanityandthebaneofmyexistence < 3; indeXESareacrimeagainsthumanityandthebaneofmyexistence++) {
-						cout << miniBox[whatever][indeXESareacrimeagainsthumanityandthebaneofmyexistence];
-					}
-					cout << endl;
-				}
-				cout << endl;*/
+				
 			}
 		}
+		
 		int counter = 0;
 		int rCoordinate = -1;
 		int cCoordinate = -1;
@@ -99,7 +92,6 @@ void solver(int num, int boxNum) {
 				}
 			}
 		}
-		/*cout << counter << endl;*/
 		// insert value if only one possible spot
 		if (counter == 1){
 			sudoku[rCoordinate + (boxNum/3)*3][cCoordinate + (boxNum%3)*3] = num;
@@ -119,19 +111,43 @@ int main() {
 				list[sudoku[i][j]].count++;
 				list[sudoku[i][j]].row[i] = true;
 				list[sudoku[i][j]].column[j] = true;
+
+				if(i>=0&&i<=2&&j>=0&&j<=2){
+					list[sudoku[i][j]].box[0] = true;
+				} else if(i>2&&i<=5&&j>=0&&j<=2){
+					list[sudoku[i][j]].box[1] = true;
+				} else if(i>5&&i<=8&&j>=0&&j<=2){
+					list[sudoku[i][j]].box[2] = true;
+				}else if(i>=0&&i<=2&&j>2&&j<=5){
+					list[sudoku[i][j]].box[3] = true;
+				}else if(i>2&&i<=5&&j>2&&j<=5){
+					list[sudoku[i][j]].box[4] = true;
+				}else if(i>5&&i<=8&&j>2&&j<=5){
+					list[sudoku[i][j]].box[5] = true;
+				}else if(i>=0&&i<=2&&j>5&&j<=8){
+					list[sudoku[i][j]].box[6] = true;
+				}else if(i>2&&i<=5&&j>5&&j<=8){
+					list[sudoku[i][j]].box[7] = true;
+				}else if(i>5&&i<=8&&j>5&&j<=8){
+					list[sudoku[i][j]].box[8] = true;
+				}
 			}
 		}
 	}
 
-	/*for (int n = 1; n < 10; n++) {
-		printNum(n);
-	}*/
-
+	// before solve
 	printSudoku();
-	for (int j = 0; j < 9; j++) {
-		solver(j+1,j);
+	
+	for (int i = 0; i < 1000; i++){
+		for (int num = 1; num <= 9; num++) {
+				for (int box = 0; box < 9; box++) {
+					solver(num,box);
+			}
+		}
 	}
+
 	cout << endl << endl << endl;
+	// after solve
 	printSudoku();
 
 	return 0;
@@ -140,4 +156,6 @@ int main() {
 
 /*TEST
 5 3 4 6 7 8 9 1 2 6 7 2 1 9 5 0 4 8 0 9 8 3 4 0 5 6 7 8 5 9 7 6 1 4 2 3 0 2 6 8 0 3 7 9 1 7 1 3 9 2 4 8 5 0 9 6 1 5 3 7 2 8 4 2 8 0 4 1 9 6 3 5 3 4 5 2 0 6 1 7 0
+
+0 8 0 0 0 0 2 0 0 0 0 0 0 8 4 0 9 0 0 0 6 3 2 0 0 1 0 0 9 7 0 0 0 0 8 0 8 0 0 9 0 3 0 0 2 0 1 0 0 0 0 9 5 0 0 7 0 0 4 5 8 0 0 0 3 0 7 1 0 0 0 0 0 0 8 0 0 0 0 4 0
 */
